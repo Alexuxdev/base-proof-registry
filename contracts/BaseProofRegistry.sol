df239a7f39ffed9a7ece8a03e1bd8fd9fd95c6ae
Alexuxdev <158582234+Alexuxdev@users.noreply.github.com>
2026-03-23 17:13:30 +0300

    Update BaseProofRegistry.sol
    define license struct for usage permissions

--- contracts/BaseProofRegistry.sol
+++ contracts/BaseProofRegistry.sol
@@ -4,6 +4,30 @@ pragma solidity ^0.8.20;
 contract BaseProofRegistry {
     address public registryOwner;
 
+    struct Asset {
+        uint256 id;
+        uint256 rootId;
+        uint256 parentId;
+        address author;
+        address currentOwner;
+        bytes32 contentHash;
+        uint64 createdAt;
+        bool revoked;
+        string uri;
+    }
+
+    struct License {
+        uint256 id;
+        uint256 assetId;
+        address licensor;
+        address licensee;
+        uint64 validFrom;
+        uint64 validUntil;
+        bool exclusive;
+        bool revoked;
+        string termsURI;
+    }
+
     constructor() {
         registryOwner = msg.sender;
     }
