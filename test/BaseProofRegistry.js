994ac84a204e69d8cfe0752cff3ca6d37661bcc9
Alexuxdev <158582234+Alexuxdev@users.noreply.github.com>
2026-04-02 17:49:32 +0300

    Update BaseProofRegistry.js
    add test for original asset registration

--- test/BaseProofRegistry.js
+++ test/BaseProofRegistry.js
@@ -18,4 +18,27 @@ describe("BaseProofRegistry", function () {
     expect(await registry.owner()).to.equal(owner.address);
     expect(await registry.paused()).to.equal(false);
   });
+
+  it("should register an original asset", async function () {
+    const { registry, owner } = await loadFixture(deployRegistryFixture);
+
+    const assetId = ethers.id("asset-1");
+    const canonicalHash = ethers.id("hash-1");
+    const metadataURI = "ipfs://asset-1";
+
+    await registry.registerOriginal(assetId, canonicalHash, metadataURI);
+
+    const asset = await registry.getAsset(assetId);
+
+    expect(asset[0]).to.equal(assetId);
+    expect(asset[1]).to.equal(canonicalHash);
+    expect(asset[2]).to.equal(assetId);
+    expect(asset[3]).to.equal(ethers.ZeroHash);
+    expect(asset[4]).to.equal(metadataURI);
+    expect(asset[5]).to.equal(owner.address);
+    expect(asset[7]).to.equal(false);
+    expect(asset[8]).to.equal(true);
+
+    expect(await registry.isCanonicalHashUsed(canonicalHash)).to.equal(true);
+  });
 });
